ed8ab9d4e548c29a1786fb2bb905cff8d6eccfba
imaansz <ims44@umd.edu>
2025-04-12 17:41:29 -0400

    added jump and run animations
    CHANGBINNNNNN

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour {
     private Rigidbody2D body;
     [SerializeField] private float speed;
+    private Animator anim;
+    private bool grounded;
 
     private void Awake() {
+
+        // Grab reference for rigidbody and animator
         body = GetComponent<Rigidbody2D>();
-        //anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
 
     } 
 
     private void Update() {
         float horizontalInput = Input.GetAxis("Horizontal"); 
+
+        // move left and right + speed
         body.linearVelocity = new Vector2((horizontalInput * speed), body.linearVelocity.y);
         
         // Flip player when moving left-right
@@ -23,10 +29,29 @@ public class playerMovement : MonoBehaviour {
             transform.localScale = new Vector3(-0.2f, 0.15f, 1);
         }
 
-
-        if(Input.GetKey(KeyCode.Space)) { 
-            body.linearVelocity = new Vector2(body.linearVelocity.x, speed/2);
+        // jump
+        if(Input.GetKey(KeyCode.Space) && grounded) { 
+            Jump();
         }
 
+        // Set animator parameters
+        anim.SetBool("run", (horizontalInput != 0));   
+        anim.SetBool("ground", grounded);
+
     }
+
+    private void Jump() {
+        body.linearVelocity = new Vector2(body.linearVelocity.x, speed/2);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "Ground") {
+            grounded = true;
+        }
+    }
+
+
+
+
 }
