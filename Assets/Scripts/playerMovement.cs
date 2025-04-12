48169ffd2264cd181964f94d1de41875bc72b007
imaansz <ims44@umd.edu>
2025-04-12 16:43:20 -0400

    added asset to player + flip when changing direction
    all the girls are girling

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -6,12 +6,24 @@ public class playerMovement : MonoBehaviour {
 
     private void Awake() {
         body = GetComponent<Rigidbody2D>();
+        //anim = GetComponent<Animator>();
 
     } 
 
     private void Update() {
-        body.linearVelocity = new Vector2((Input.GetAxis("Horizontal") * speed), body.linearVelocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal"); 
+        body.linearVelocity = new Vector2((horizontalInput * speed), body.linearVelocity.y);
         
+        // Flip player when moving left-right
+        // scale is x: 0.2, y: 0.15 bc asset is very large
+        if (horizontalInput > 0.01f) {
+            transform.localScale = new Vector3(0.2f, 0.15f, 1);
+        }
+        else if (horizontalInput <= -0.01f) {
+            transform.localScale = new Vector3(-0.2f, 0.15f, 1);
+        }
+
+
         if(Input.GetKey(KeyCode.Space)) { 
             body.linearVelocity = new Vector2(body.linearVelocity.x, speed/2);
         }
