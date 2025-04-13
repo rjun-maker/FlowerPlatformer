6f68a40c19db2a71c295b80c8237d1475711e7fe
imaansz <ims44@umd.edu>
2025-04-13 00:11:39 -0400

    added assets to level

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -8,13 +8,15 @@ public class cameraController : MonoBehaviour {
     [SerializeField] private Transform player;
     [SerializeField] private float aheadDistance;
     [SerializeField] private float cameraSpeed;
-    private float lookAhead;
+    private float lookAheadX;
+    private float lookAheadY;
 
 
     private void Update() {
        // transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
-        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
-        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+        transform.position = new Vector3(player.position.x + lookAheadX, (player.position.y/5) + lookAheadY, transform.position.z);
+        lookAheadX = Mathf.Lerp(lookAheadX, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+        lookAheadY = Mathf.Lerp(lookAheadY, (aheadDistance * player.localScale.y), Time.deltaTime * cameraSpeed);
     }
 
 
