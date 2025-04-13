af6630178249c3bff1c180f3dbf1ff93a08c1a9d
imaansz <ims44@umd.edu>
2025-04-12 22:09:56 -0400

    changbin room

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -2,17 +2,23 @@ using UnityEngine;
 
 public class cameraController : MonoBehaviour {
    
-   [SerializeField] private float speed;
-   private float currentPosX;
-   private Vector3 velocity = Vector3.zero;
+    [SerializeField] private float speed;
+    private float currentPosX;
+    private Vector3 velocity = Vector3.zero;
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
 
 
-    private void update() {
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed * Time.deltaTime);
+    private void Update() {
+       // transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
     }
 
 
-    private void moveToNewRoom(Transform _newRoom) {
+    public void MoveToNewRoom(Transform _newRoom) {
         currentPosX = _newRoom.position.x;
 
     }
--- Assets/Scripts/door.cs
+++ Assets/Scripts/door.cs
@@ -3,6 +3,21 @@ using UnityEngine;
 public class door : MonoBehaviour {
     [SerializeField] private Transform previousRoom;
     [SerializeField] private Transform nextRoom;
+    [SerializeField] private cameraController cam;
+
+    private void OnTriggerEnter2D (Collider2D collision) {
+        if (collision.tag == "Player") {
+            print("trigger activated");
+            
+            if (collision.transform.position.x < transform.position.x) {
+                cam.MoveToNewRoom(nextRoom);
+
+            } else {
+                cam.MoveToNewRoom(previousRoom);
+            }
+        }
+    }
+
 
 
 }
